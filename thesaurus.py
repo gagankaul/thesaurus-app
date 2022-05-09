df9f93151f1d36c1584db4057517dbb1d51cf835
Gagan Kaul <gagankaul@gmail.com>
2022-05-09 09:55:50 -0400

    updated

--- thesaurus.py
+++ thesaurus.py
@@ -1,12 +1,14 @@
 #A Thesaurus application framework using dictionaries
 
+import random
+
 print("Welcome to the Thesaurus App")
 
 thesaurus = {
-    "hot":[],
-    "cold": [],
-    "happy": [],
-    "sad": [],
+    "hot":['balmy', 'summery', 'tropical', 'boiling', 'scorching'],
+    "cold":['chilly', 'cool', 'freezing', 'frigid', 'polar'],
+    "happy":['content', 'cheery', 'merry', 'jovial', 'jocular'],
+    "sad":['unhappy', 'downcast', 'miserable', 'glum', 'melancholy'],
 }
 
 print("\nChoose a word from the thesaurus and I will give you a synonym.")
@@ -15,5 +17,22 @@ print("\nHere are the words in the thesaurus:")
 for key in thesaurus.keys():
     print("\t-" + key)
 
-word_pick = input("\nWhat word would you like a synonym for: ")
+word_pick = input("\nWhat word would you like a synonym for: ").lower().strip()
+random_index = random.randint(0,4)
+print("A synonym for " + word_pick + " is " + thesaurus[word_pick][random_index] + ".")
+
+choice = input("\nWould you like to see the whole thesaurus (y/n): ").lower().strip()
+
+if choice.startswith("y"):
+    for key,value in thesaurus.items():
+        print("\n" + key.title() + " synonyms are:")
+        for each in value:
+            print("\t- " + each)
+else:
+    print("I hope you enjoyed the program. Thank you!")
+
+
+
+
+
 
