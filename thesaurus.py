83b95b2278ed06ed8c39d29b103ff73d7f81ff63
Gagan Kaul <gagankaul@gmail.com>
2022-05-09 10:05:44 -0400

    add comments

--- thesaurus.py
+++ thesaurus.py
@@ -1,9 +1,8 @@
-#A Thesaurus application framework using dictionaries
+#A Thesaurus app framework based on dictionaries
 
 import random
 
-print("Welcome to the Thesaurus App")
-
+#Define the thesaurus
 thesaurus = {
     "hot":['balmy', 'summery', 'tropical', 'boiling', 'scorching'],
     "cold":['chilly', 'cool', 'freezing', 'frigid', 'polar'],
@@ -11,28 +10,28 @@ thesaurus = {
     "sad":['unhappy', 'downcast', 'miserable', 'glum', 'melancholy'],
 }
 
+#Print welcome message
+print("Welcome to the Thesaurus App")
 print("\nChoose a word from the thesaurus and I will give you a synonym.")
-
 print("\nHere are the words in the thesaurus:")
 for key in thesaurus.keys():
     print("\t-" + key)
 
+#Ask the user to choose a word
 word_pick = input("\nWhat word would you like a synonym for: ").lower().strip()
+
+#Display a random synonym for the chosen word
 random_index = random.randint(0,4)
 print("A synonym for " + word_pick + " is " + thesaurus[word_pick][random_index] + ".")
 
+#Get user input
 choice = input("\nWould you like to see the whole thesaurus (y/n): ").lower().strip()
 
+#Display results based on user input
 if choice.startswith("y"):
     for key,value in thesaurus.items():
         print("\n" + key.title() + " synonyms are:")
         for each in value:
             print("\t- " + each)
 else:
-    print("I hope you enjoyed the program. Thank you!")
-
-
-
-
-
-
+    print("\nI hope you enjoyed the program. Thank you!")
